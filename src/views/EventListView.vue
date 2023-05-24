6dfc4b4c4376517d2356c30d25000aa4b3266fbf
Nitin <132347798+nitin8857888@users.noreply.github.com>
2023-05-24 17:36:15 +0530

    events from db.json server

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,41 +1,21 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
-import { ref } from 'vue'
-const events = ref([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline  friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28,2022',
-    time: '12:00',
-    petsallowed: true,
-    organizer: 'Kat Laydee',
-  },
-  {
-    id: 4582797,
-    category: 'food',
-    title: 'Community gardeneing',
-    description: 'join us we tend to the community edible plants.',
-    location: 'Flora city',
-    date: 'March 14,2022',
-    time: '10:00',
-    petsallowed: true,
-    organizer: 'fern pollins',
-  },
-  {
-    id: 8419998,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'January 22,2022',
-    time: '11:00',
-    petsallowed: false,
-    organizer: 'carey Wales',
-  },
-])
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+const events = ref(null)
+onMounted(() => {
+  axios
+    .get(
+      'https://my-json-server.typicode.com/nitin8857888/real-world-vue/events'
+    )
+    .then((response) => {
+      events.value = response.data
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 </script>
 
 <template>
